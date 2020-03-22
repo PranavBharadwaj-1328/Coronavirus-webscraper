e9713828cf21d0e28732f87551693da4895d3a1f
PranavBharadwaj-1328 <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2020-03-22 10:58:13 +0530

    Update corona.php

--- corona.php
+++ corona.php
@@ -14,8 +14,10 @@
   }
   $infected =  trim($stat," - Worldometer");
   echo "<title> Coronastats </title>";
+  echo "<center>";
   $a =  explode(" ",$infected);
   echo("<br><br><br><br><h2 style='color:red;'>Reported Cases: ".$a[3]."</h2><br/>");
   echo("<h2 style='color:red;'>Total Deaths: ".$a[6]."</h2><br />");
   echo('<iframe style="align:center;" width="600" height="400" src="https://www.youtube.com/embed/1APwq1df6Mw"/>')
+  echo "</center>";
 ?>
