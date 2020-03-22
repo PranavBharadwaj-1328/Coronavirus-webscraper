3303bd2ee86ba744afc10dcfd91ea34956cf76d0
PranavBharadwaj-1328 <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2020-03-22 10:14:33 +0530

    Add files via upload

---
+++ corona.php
@@ -0,0 +1,21 @@
+<?php
+
+  $main_url = "https://www.worldometers.info/coronavirus/";
+  $str = file_get_contents($main_url);
+  if(strlen($str)>0)
+  {
+    $str = trim(preg_replace('/\s+/', ' ', $str));
+    preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title);
+  }
+  $stat = "";
+  foreach ($title as $t)
+  {
+    $stat = $stat.$t;
+  }
+  $infected =  trim($stat," - Worldometer");
+  echo "<title> Coronastats </title>";
+  $a =  explode(" ",$infected);
+  echo("<br><br><br><br><h2 style='color:red;'>Reported Cases: ".$a[3]."</h2><br/>");
+  echo("<h2 style='color:red;'>Total Deaths: ".$a[6]."</h2><br />");
+  echo('<iframe style="align:center;" width="600" height="400" src="https://www.youtube.com/embed/1APwq1df6Mw"/>')
+?>
